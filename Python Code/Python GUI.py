dcd379d3f239e4a32e3032b788298feab1b983fd
Jesse <jesse.tijsma@gmail.com>
2016-11-07 10:34:06 +0100

    Python GUI Code
    De code van de Python GUI code om de Arduino aan te sturen.

---
+++ Python Code/Python GUI.py
@@ -0,0 +1,13 @@
+#GUI for Arduino 
+
+from tkinter import *
+
+#create the window
+root = Tk()
+
+#modify root window 
+root.title("Arduino GUI")
+root.geometry("800x400")
+
+#Kick off event loop
+root.mainloop()
