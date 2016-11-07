96ec25ec2f3dfd012edd52ee9033b39ceb798c25
Markcreator <markmediahorn@hotmail.com>
2016-11-07 15:16:22 +0100

    Fixed serial seaeching + mutliboard support

--- Centrale/serialThread.py
+++ Centrale/serialThread.py
@@ -4,23 +4,25 @@ import serial
 
 class SerialThread (threading.Thread):
 
-	ports = [None] * 5;
+	ports = [None] * 100;
 
 	def readCommand(self, port):
 		length = ord(port.read(1).decode('utf-8')) # Get command length
 		return port.read(length).decode('utf-8') # Get entire command with length
-		
+			
 	def update(self): # Connect, disconnect and process commands
 		for i in range(len(self.ports)):
-			try:		
+			try:	
 				if self.ports[i] == None:
-					self.ports[i] = serial.Serial(port="COM" + str(i + 3),baudrate=9600) # Connect to board
+					try:
+						self.ports[i] = serial.Serial(port="COM" + str(i),baudrate=9600) # Connect to board
+					except: continue
 					
 				else:
-					print("COM" + str(i + 3) + ": " + self.readCommand(self.ports[i]))
+					print("COM" + str(i) + ": " + self.readCommand(self.ports[i])) # Print board and recieved command
 					
 			except: self.ports[i] = None
-		
+				
 		threading.Timer(1, self.update).start() # Try again in 1 second
 
 	#
